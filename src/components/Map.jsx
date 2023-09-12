57e5114a47afdaa22f9b6f690fcb7675a39ec5d3
Tilacin <tilacin45@gmail.com>
2023-09-12 20:22:48 +0300

    Настроили определение геопозиции пользователя на карте

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,18 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from "react-leaflet";
 import { useCities } from "../contexts/CitiesContext";
+import { useEffect, useState } from "react";
+import { useGeolocation } from "../hooks/useGeolocation";
 
 import styles from "./Map.module.css";
-import { useEffect, useState } from "react";
+import Button from "./Button";
 
 function Map() {
 
   const { cities } = useCities();
-
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams] = useSearchParams();
+  const { isLoading: isLoadingPosition, position: geolocationPosition, getPosition } = useGeolocation()
 
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
@@ -19,8 +21,15 @@ function Map() {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng])
   }, [mapLat, mapLng])
 
+  useEffect(function () {
+    if (geolocationPosition) setMapPosition([geolocationPosition.lat, geolocationPosition.lng])
+  }, [geolocationPosition])
+
   return (
     <div className={ styles.mapContainer }>
+      { !geolocationPosition && <Button type='position' onClick={ getPosition }>
+        { isLoadingPosition ? 'Загрузка' : 'Ваше местоположение' }
+      </Button> }
       <MapContainer
         center={ mapPosition }
         zoom={ 6 }
