de5f484d15b94e76aadd30dca1886e671b7bc6d1
Tilacin <tilacin45@gmail.com>
2023-09-14 23:24:59 +0300

    Начали переписывать состояния н а редюсер

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,26 +1,65 @@
-import { createContext, useState, useEffect, useContext } from "react";
+import { createContext, useState, useEffect, useContext, useReducer } from "react";
 
 const BASE_URL = 'http://localhost:8000'
 
 const CitiesContext = createContext()
 
+const initialState = {
+    cities: [],
+    isLoading: false,
+    currentCity: {},
+    error: "",
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'loading':
+            return {
+                ...state,
+                isLoading: true,
+            }
+        case 'cities/loaded':
+            return {
+                ...state,
+                isLoading: false,
+                cities: action.payload
+            }
+        case 'cities/created':
+
+        case 'cities/deleted':
+
+        case 'rejected':
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload,
+            }
+
+        default:
+            throw new Error("Неизвестный тип действия")
+    }
+}
+
 function CitiesProvider({ children }) {
-    const [cities, setCities] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
-    const [currentCity, setCurrentCity] = useState({})
+    const [{ cities, isLoading, currentCity }, dispatch] = useReducer(reducer, initialState)
+    // const [cities, setCities] = useState([])
+    // const [isLoading, setIsLoading] = useState(false)
+    // const [currentCity, setCurrentCity] = useState({})
 
     useEffect(function () {
         async function fetchCities() {
+            dispatch({ type: "loading" })
             try {
-                setIsLoading(true)
+
                 const res = await fetch(`${BASE_URL}/cities`)
                 const data = await res.json()
-                setCities(data)
+                dispatch({ type: 'cities/loaded', payload: data })
             }
             catch {
-                alert("Произошла ошибка при загрузке данных...")
-            } finally {
-                setIsLoading(false)
+                dispatch({
+                    type: 'rejected',
+                    payload: "Произошла ошибка при загрузке данных...",
+                })
             }
         }
         fetchCities()
